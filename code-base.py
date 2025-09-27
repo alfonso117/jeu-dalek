e4a6a1120b0883df8b43565602efbd985eebe888
alfonso117 <carlosalfonso1997@hotmail.com>
2025-09-26 22:33:33 -0400

    cambios en move doc y while al momento de guardar position del doc

--- code-base.py
+++ code-base.py
@@ -10,6 +10,10 @@
 # print(Style.BRIGHT + "Texte en style BRIGHT")
 # print(Style.RESET_ALL + "Retour au style normal")
 
+import os
+import time
+from collections import defaultdict
+
 from enum import Enum
 from colorama import init, Fore, Back, Style
 import msvcrt
@@ -23,9 +27,6 @@ def gotoxy(x, y):
 positionInitialX, positionInitialY = 15, 3
 
 class Action(Enum):
-    from enum import Enum
-## aqui
-class Action(Enum):
     UP_LEFT = 'G'
     UP = 'H'
     UP_RIGHT = 'I'
@@ -36,11 +37,12 @@ class Action(Enum):
     DOWN_RIGHT = 'Q'
     TELETRANSPORTE = 't'
     ZAPPER = 'z'
-## hasta aca
+
 class Case(Enum):
     Doctor = 0
     Dalek = 1
     CVide = 2
+    Cjunk = 3
 
 grille = [
     [Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, Case.CVide, ],
@@ -73,17 +75,17 @@ class LC:
 
 class Move:
     def __init__(self, from_position, to_position):
-        self.from_position = from_position
-        self.to_position = to_position
+        self.from_position:LC = from_position
+        self.to_position:LC = to_position
 
 grilleColors = {
     Case.Doctor: caracteristiques(Back.BLUE, ' D '),
     Case.Dalek: caracteristiques(Back.RED, ' & '),
-    Case.CVide: caracteristiques(Back.WHITE, ' _ ')
+    Case.CVide: caracteristiques(Back.WHITE, ' _ '),
+    Case.Cjunk: caracteristiques(Back.YELLOW, ' X ')
 }
 
-gotoxy(positionInitialX, positionInitialY)
-
+## Print of the grill
 for y, ligne in enumerate(grille):
 
     gotoxy(positionInitialX, positionInitialY + y)
@@ -92,11 +94,11 @@ for y, ligne in enumerate(grille):
         objet = grilleColors[case]
         print(objet.color + objet.char, end='')
 
-runGame = True
-win = False
-lose = False
-niveau = 1
-## aqui 
+## Characters positions in the grille
+doc_position = []
+dalek_pos = []
+
+## Read arrow key
 def lire_touche(m, touche):
     if touche == Action.UP_LEFT:
         m.to_position.l -= 1
@@ -119,12 +121,13 @@ def lire_touche(m, touche):
     elif touche == Action.RIGHT:
         m.to_position.c += 1
     elif touche == Action.TELETRANSPORTE:
-        m.to_position.l = random.randint(0, len(grille) - 1)
-        m.to_position.c = random.randint(0, len(grille[0]) - 1)
-## hasta aca
+       teletransporter(m)
+    elif touche == Action.ZAPPER:
+        zapp()
 
 
-def move_doc(m, grille, positionInitialX, positionInitialY ):
+## Move doctor 
+def move_doc(m:Move, grille, positionInitialX, positionInitialY ):
     if (0 <= m.to_position.l < len(grille) and (0 <= m.to_position.c < len(grille[0])) and (grille[m.to_position.l][m.to_position.c] == Case.CVide)):
                     
         ## look for last position and Print new case bckGround
@@ -141,50 +144,162 @@ def move_doc(m, grille, positionInitialX, positionInitialY ):
 
         ## Last position == actual position
         m.from_position = LC(m.to_position.l, m.to_position.c)
+        doc_position = [m.from_position.l, m.from_position.c]
 
+## get random Position
+def random_position():
+    ## Give random position to the Dalek
+    x = random.randint(0, len(grille) - 1)
+    y = random.randint(0, len(grille[0]) - 1)
+
+    return x, y
+
+def teletransporter(m:Move):
+    m.to_position.l , m.to_position.c = random_position()
+    move_doc(m, grille, positionInitialX, positionInitialY )
+   
+
+## Print one Dalek
+def print_dalek(grille, l_dalek, c_dalek ):
+        ## if the space in the Grille is empty print the fcking Dalek
+        if grille[l_dalek][c_dalek] == Case.CVide:
+            grille[l_dalek][c_dalek] = Case.Dalek
+            ## GO and print new position
+            gotoxy(positionInitialX + c_dalek * 3, positionInitialY + l_dalek)
+            print(grilleColors[Case.Dalek].color + grilleColors[Case.Dalek].char, end='')
+           
+            ## Save 1 dalek position in the list of dalekssss
+            dalek_pos.append([l_dalek, c_dalek]) 
+        elif grille[l_dalek][c_dalek] == Case.Dalek:
+            grille[l_dalek][c_dalek] = Case.Cjunk
+
+            gotoxy(positionInitialX + c_dalek * 3, positionInitialY + l_dalek)
+            print(grilleColors[Case.Cjunk].color + grilleColors[Case.Cjunk].char, end='')
+
+
+## Print all daleks
+def dessin_daleks(nbDaleks, niveau, grille):
+        for i in range(nbDaleks + niveau):
+            x, d = random_position()
+            print_dalek(grille, x, d)
+
+
+def if_lose():
+    os.system('cls')
+    print(Fore.RED + "YOU LOST!!! :(")
+    time.sleep(3)
+
+## Move daleks based in doc's position 
+def move_daleks(dalek_pos, m:Move):
+    global lose
+    new_positions = []
+
+    for i, (x, y) in enumerate(dalek_pos):
+        if grille[x][y] != Case.Cjunk:
+            gotoxy(positionInitialX + y * 3, positionInitialY + x)
+            print(grilleColors[Case.CVide].color + grilleColors[Case.CVide].char, end='')
+            grille[x][y] = Case.CVide
+
+        # Mover en dirección al Doctor
+        if x < m.to_position.l:
+            x += 1
+        elif x > m.to_position.l:
+            x -= 1
+
+        if y < m.to_position.c:
+            y += 1
+        elif y > m.to_position.c:
+            y -= 1
+
+        if x == m.to_position.l and y == m.to_position.c:
+            if_lose()
+            break
+
+
+        new_positions.append((x, y))
+
+    # Verificar colisiones y actualizar grilla
+    dalek_pos.clear()
+    positions_seen = {}
+
+    for (x, y) in new_positions:
+        if (x, y) in positions_seen:
+            # Colisión → basura
+            grille[x][y] = Case.Cjunk
+            gotoxy(positionInitialX + y * 3, positionInitialY + x)
+            print(grilleColors[Case.Cjunk].color + grilleColors[Case.Cjunk].char, end='')
+
+        else:
+            # Dalek válido
+            positions_seen[(x, y)] = True
+            grille[x][y] = Case.Dalek
+            gotoxy(positionInitialX + y * 3, positionInitialY + x)
+            print(grilleColors[Case.Dalek].color + grilleColors[Case.Dalek].char, end='')
+            dalek_pos.append((x, y))
+
+## Verify if the level is passed
+def level_win():
+    if len(dalek_pos) == 0:
+        win = True
+        niveau += 1
+        os.system('cls') 
+        print(Fore.GREEN + "You WON!!!!!")
+
+        time.sleep(3)
+
+def zapp():
+    directions = [(-1,-1), (-1,0), (-1,1),
+                  ( 0,-1),         ( 0,1),
+                  ( 1,-1), ( 1,0), ( 1,1)]
+
+    rows = len(grille)
+    cols = len(grille[0])
+    x, y = doc_position
+
+    for direct_x, direct_y in directions:
+        nx = x + direct_x
+        ny = y + direct_y
+        if 0 <= nx < rows and 0 <= ny < cols:
+            if grille[nx][ny] == Case.Dalek:
+                # Convertir en Junk en la grilla
+                grille[nx][ny] = Case.Cjunk
+                gotoxy(positionInitialX + ny * 3, positionInitialY + nx)
+                print(grilleColors[Case.Cjunk].color + grilleColors[Case.Cjunk].char, end='')
+
+                # Eliminar también de dalek_pos si está ahí
+                if (nx, ny) in dalek_pos:
+                    dalek_pos.remove((nx, ny))
+
+gotoxy(positionInitialX, positionInitialY)
+runGame = True
+win = False
+lose = False
+niveau = 1
+dalekInGame = 2
+
+## Le jeu
 while runGame:
-
+    win = False
+    lose = False
+    ## Give the Doctor a random Position
     l = random.randint(0, len(grille) - 1)
     c = random.randint(0, len(grille[0]) - 1)
+    ## Save his positon on the grille 
+    grille[l -1][c -1 ] = Case.Doctor
 
-    grille[l][c] = Case.Doctor
+    ## Print the Doctor (console) 
     gotoxy(positionInitialX + c * 3, positionInitialY + l)
     print(grilleColors[Case.Doctor].color + grilleColors[Case.Doctor].char, end='')
 
-    m = Move(LC(l, c),LC(l, c))
+    ## Save his position in a new variable
+    m = Move(LC(l-1, c-1),LC(l-1, c-1))
+    doc_position = [m.from_position.l -1, m.from_position.c -1] ## Position [x,y] actuellement
+    print(doc_position)
+    dessin_daleks(dalekInGame, niveau, grille)              
 
-    dalekInGame = 3
-
-    doctor_xy = [0,0]
-
-    dalek_pos = [[0,0],[0,0],[0,0]]
-    ## Dessin daleks 
-    ##for i in range(dalekInGame * niveau):
-        ##dalek_pos = [l_dalek , c_dalek]
-
-
-        ## Give random position to the Dalek
-       ## dalek_pos[i] = random.randint(0, len(grille) - 1)
-        ##c_dalek[i] = random.randint(0, len(grille[0]) - 1)
-
-        ## if the space in the Grille is empty print the fcking Dalek
-        ##if grille[l_dalek][c_dalek] == Case.CVide:
-           ## grille[l_dalek][c_dalek] = Case.Dalek
-            ##gotoxy(positionInitialX + c_dalek * 3, positionInitialY + l_dalek)
-            ##print(grilleColors[Case.Dalek].color + grilleColors[Case.Dalek].char, end='')
-        ## else no sabemos todavia 
-
-
-    def move_daleks():
-        for _ in range(dalekInGame * niveau):
-            ## look for last position and Print new case bckGround
-            gotoxy(positionInitialX + m.from_position.c * 3, positionInitialY + m.from_position.l)
-            print(grilleColors[Case.CVide].color + grilleColors[Case.CVide].char, end='')
-             
-## aca
     while not win and not lose:
         touche = msvcrt.getch()
-        m.to_position = LC(m.from_position.l, m.from_position.c)
+        m.to_position = LC(m.from_position.l -1, m.from_position.c -1)
 
         if touche == b'\xe0':
             touche = msvcrt.getch().decode("utf-8")
@@ -195,7 +310,11 @@ while runGame:
             action = Action(touche)
             lire_touche(m, action)
             move_doc(m, grille, positionInitialX, positionInitialY)
+            move_daleks(dalek_pos, m)
+            print(doc_position)
+            
+            level_win()
+
+            
         except ValueError:
             continue 
-## hasta aqui
-
